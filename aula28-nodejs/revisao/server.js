c261e4e32369db7b0d01a787e8046dd44eeb02f0
beacastroalves <beacastro1008@gmail.com>
2025-07-08 16:06:24 +0100

    Exercicio - Desafio

--- aula28-nodejs/revisao/server.js
+++ aula28-nodejs/revisao/server.js
@@ -4,10 +4,13 @@ const host = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.write('Ola Nodejs');
+  res.write('<h1>');
+  res.write('Voce ');
+  res.write('Conseguiu!');
+  res.write('</h1>');
   res.end();
 });
 
 server.listen(port, host, () => {
-  console.log(`http://${host}:${port}`);
+  console.log(`http://${host}:${port} Você conseguiu!`);
 });

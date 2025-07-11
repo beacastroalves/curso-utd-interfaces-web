e0d4e9d4fc74c24e7377430d9724181369fe9042
beacastroalves <beacastro1008@gmail.com>
2025-07-11 10:32:10 +0100

    Nodejs crash course - parte final - Demos

---
+++ aula30-node-crash-course-complementar/osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// userIndo()
+console.log(os.userInfo().username);
+
+// totalmem()
+console.log(os.totalmem());
+
+// freememe()
+console.log(os.freemem());
+
+// cpus()
+console.log(os.cpus());
